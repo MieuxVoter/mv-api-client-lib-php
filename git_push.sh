daf8a9941b45d99c3b3f1ca43b76a61064326936
domi41 <dominique.merle.41@gmail.com>
2022-05-08 12:07:48 +0200

    Minor update

--- git_push.sh
+++ git_push.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 # ref: https://help.github.com/articles/adding-an-existing-project-to-github-using-the-command-line/
 #
-# Usage example: /bin/sh ./git_push.sh "" "" "" "minor update"
-# Usage example: /bin/sh ./git_push.sh gitlab.com wing328 openapi-petstore-perl "minor update"
+# Usage example: /bin/sh ./git_push.sh "" "" "" "" "minor update"
+# Usage example: /bin/sh ./git_push.sh gitlab.com wing328 openapi-petstore-perl main "minor update"
 
-git_host=$3
-git_user_id=$1
-git_repo_id=$2
-release_note=$4
+git_host=$1
+git_user_id=$2
+git_repo_id=$3
+git_branch=$4
+release_note=$5
 
 if [ "$git_host" = "" ]; then
     git_host="github.com"
@@ -24,6 +25,11 @@ if [ "$git_repo_id" = "" ]; then
     echo "[INFO] No command line input provided. Set \$git_repo_id to $git_repo_id"
 fi
 
+if [ "$git_branch" = "" ]; then
+    git_branch="master"
+    echo "[INFO] No command line input provided. Set \$git_branch to $git_branch"
+fi
+
 if [ "$release_note" = "" ]; then
     release_note="Minor update"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
@@ -36,9 +42,10 @@ git init
 git add .
 
 # We need to somehow either put this into a file or add variables to this mustache template
-# I don't won't to write in my globals, and perhaps this deserves its own user?
+# I don't won't to use my global git config, and perhaps this deserves its own user?
 # We could ensure that that user only has access to the generated lib projects and not all of MV,
 # especially considering the usage of the GITHUB_TOKEN below.
+# These could just as well be "dummy" values, unless we start signing these commits.
 git config user.name domi41
 git config user.email dominique.merle.41@gmail.com
 
@@ -58,8 +65,15 @@ if [ "$git_remote" = "" ]; then # git remote not defined
 
 fi
 
-git pull origin master
+git pull origin $git_branch
 
-# Pushes (Forces) the changes in the local repository up to the remote repository
-echo "Git pushing to https://${git_host}/${git_user_id}/${git_repo_id}.git"
-git push origin master 2>&1 | grep -v 'To https'
+# It's too easy to fail, with force ; just make sure we're targeting a project
+# whose name starts with `mv-api-client-lib-`.
+if [[ "$git_repo_id" =~ ^mv-api-client-lib- ]]; then
+    # Pushes (Forces) the changes in the local repository up to the remote repository
+    echo "Git pushing to https://${git_host}/${git_user_id}/${git_repo_id}.git"
+    git push --force origin $git_branch
+else
+    echo "[ERROR] $git_repo_id is not a repo starting with mv-api-client-lib-…"
+fi
+
